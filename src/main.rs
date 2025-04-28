2ca29ebe4c558efd652ad5fb2447d0b7a0495048
trenton117 <trenton117@protonmail.com>
2025-04-28 19:11:04 -0400

    implemented add_item, show_inventory, and remove_item functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
 fn main() {
-    println!("Hello, world!");
-    println!("This is my inventory project.");
 
 // Plan
 // 1. Create an empty inventory (a vector of strings)
 // 2. Make a function to add items to inventory
 // 3. Make a function to remove items from inventory
 // 4. Make a function to list all items in inventory
-// 5. this is just a commit test
-// 6. test number 2
+
+    let mut inventory: Vec<String> = Vec::new();
+
+    add_item(&mut inventory, "Sword".to_string());
+    add_item(&mut inventory, "Shield".to_string());
+
+    show_inventory(&inventory);
+
+    remove_item(&mut inventory, "Sword".to_string());
+
+    show_inventory(&inventory);
 }
+
+fn add_item(inventory: &mut Vec<String>, item: String) {
+    inventory.push(item);
+}
+
+fn remove_item(inventory: &mut Vec<String>, item: String) {
+    if let Some(pos) = inventory.iter().position(|x| *x == item) {
+        inventory.remove(pos);
+    } else {
+        println!("Item {} not found in inventory.", item);
+    }
+}
+
+fn show_inventory(inventory: &Vec<String>) {
+    println!("Inventory:");
+    for item in inventory {
+        print_item(item);
+    }
+}
+
+fn print_item(item: &String) {
+    println!("{}", item);
+}
+
