c411cd9f1dc059a8356c8db54cd5e8c929660c62
trenton117 <trenton117@protonmail.com>
2025-04-28 14:09:13 -0400

    initial project setup: created cargo project, wrote plan in comments.

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+    println!("This is my inventory project.");
+
+// Plan
+// 1. Create an empty inventory (a vector of strings)
+// 2. Make a function to add items to inventory
+// 3. Make a function to remove items from inventory
+// 4. Make a function to list all items in inventory
+}
